6310f4b98ef941177940afc1719e0de0af86055b
Alex <alexandrhall@gmail.com>
2022-06-13 13:28:27 +0200

    notfound update

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-  return <>Hoppsan! Nu blev det fel.</>;
+  return (
+    <>
+      Hoppsan! Nu blev det fel. <br />
+      <Link to={"/"}>Klicka här får gå tillbaka</Link>
+    </>
+  );
 };
