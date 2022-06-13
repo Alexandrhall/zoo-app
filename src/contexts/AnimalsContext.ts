576df161e3cd449350e2470af085071ef725ba4b
Alexandrhall <alexandrhall@gmail.com>
2022-06-14 00:18:48 +0200

    fixed the code

--- src/contexts/AnimalsContext.ts
+++ src/contexts/AnimalsContext.ts
@@ -1,18 +1,1 @@
-import axios from "axios";
-import { IAnimal } from "../models/IAnimal";
-import { getList, saveLocal } from "../services/StorageService";
-
-export const getAnimals = () => {
-  let tempList: IAnimal[] = [];
-
-  tempList = getList();
-
-  if (tempList.length == 0) {
-    axios
-      .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
-      .then((response) => {
-        tempList = response.data;
-      });
-  }
-  saveLocal(tempList);
-};
+export const animalListContext = () => {};
